6646024f12ec3cc7cb5b7786569e921abf95f92c
Taylor Treece <taylor@taylortreece.com>
2022-03-27 02:20:38 -0400

    shopping routes created and tested. (carts, orders, products)

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var productsRouter = require('./routes/products');
+var ordersRouter = require('./routes/orders');
+var cartsRouter = require('./routes/carts')
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+app.use('/orders', ordersRouter);
+app.use('/carts', cartsRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Users endpoint has been reached.');
 });
 
 router.get('/:name', async (req, res, next) => {
