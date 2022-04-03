54f359d5abeec6fb899c809a6522460bc1a9466d
taylortreece <taylor@taylortreece.com>
2022-04-03 14:53:16 -0500

    User login set up. Added sanitizing and validations to be added to /auth/registration && /auth/login routes.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,53 +4,87 @@ var router = express.Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken")
 
-router.post('/login', async (req, res, next) => {
-    res.send("Login endpoint has been hit.")
-})
-
+// REGISTER
 router.post('/register', async (req, res, next) => {
     try {
         const { firstName, lastName, email, password } = req.body;
-
+        
         console.log(firstName, lastName, email, password)
-
+        
         if (!(email && password && firstName && lastName)) {
             res.status(400).send("All inputs are required");
         }
-
+        
         // check if user already exists
         // validate if user exists in our database
         const oldUser = await User.findOne({ email });
-
+        
         if (oldUser) { 
             return res.status(409).send("User already exists. Please login.");
         }
-
+        
         // encrypt user password
         encrypedUserPassword = await bcrypt.hash(password, 10);
-
+        
         const user = await User.create({
             first_name: firstName,
             last_name: lastName,
             email: email.toLowerCase(),
             password: encrypedUserPassword
         });
-
+        
         // create token
         const token = jwt.sign(
             { user_id: user._id, email },
             process.env.TOKEN_KEY,
             { expiresIn: "5h" }
-        );
+            );
+            
+            user.token = token;
+            
+            res.status(201).json(user)
+        } catch (err) {
+            
+            // Add morgan to register error
+            console.log(err)
+        }
+    })
 
-        user.token = token;
+    // LOGIN
+    router.post('/login', async (req, res, next) => {
+        try {
+            // Get user input
+            const { email, password } = req.body;
+            console.log(email, password)
+            // Validate user input
+            if (!(email && password)) {
+                res.status(400).send("All input is required");
+            }
 
-        res.status(201).json(user)
-    } catch (err) {
+            // Validate if user exist in our database
+            const user = await User.findOne({ email });
+            console.log(JSON.stringify(user))
+            if(user && (await bcrypt.compare(password, user.password))) {
+                //Create token
+                console.log("Token_KEY", process.env.TOKEN_KEY)
+                const token = jwt.sign(
+                    { user_id: user._id, email },
+                    process.env.TOKEN_KEY,
+                    {
+                        expiresIn: "24h",
+                    }
+                );
+                console.log(JSON.stringify(token))
+                // Save user token
+                user.token = token;
 
-        // Add morgan to register error
-        console.log(err)
-    }
-})
+                return res.status(200).json(user);
+            }
 
-module.exports = router;
+            return res.status(400).send("Invalid Credentials");
+        } catch(err) {
+            res.status(400).send("error")
+        }
+    })
+
+    module.exports = router;
