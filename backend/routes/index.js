48ee3dc4e71685fc8ae9aab5e3ad7075fc8202de
taylortreece <taylor@taylortreece.com>
2022-04-03 19:02:57 -0500

    added verifyToken middleware to home page for testing.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const auth = require("../src/middleware/verifyToken")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', auth, function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
