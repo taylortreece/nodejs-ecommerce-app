285507f8a5092bf6ef2c3e630399b39dcf65cc36
taylortreece <tayloratreece@gmail.com>
2022-04-12 17:23:08 -0500

    added code to app.test.js to close mongoDB connection after tests.

--- backend/app.test.js
+++ backend/app.test.js
@@ -1,20 +1,21 @@
 const request = require("supertest");
 const app = require("../backend/app");
-// const mongoDB = require("./config/database")
+const mongoose = require("mongoose")
 
-describe("Test the root path", () => {
+afterAll(done => {
+  mongoose.connection.close()
+  done()
+})
+
     test("It should response the GET method", async () => {
       const response = await request(app).get("/");
       expect(response.statusCode).toBe(200);
     });
-  });
 
-describe("Test the products path", () => {
     test("It should response the GET method", async () => {
         return await request(app)
           .get("/products")
           .expect(200)
     });
-});
 
 // add test for mongoDB Connection
--- backend/config/database.js
+++ backend/config/database.js
@@ -6,7 +6,7 @@ module.exports = {
   connect: async () => {
     try {
       await mongoose.connect(MONGO_URI);
-      console.log("DB Connection Successful.")
+      // console.log("DB Connection Successful.")
     } catch(err) {
       console.log("DB Connection Error: ", err)
     }
