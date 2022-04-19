8e53dfab5224fbdaa8222e3ceb399d0f02a910d4
Taylor <taylor@taylortreece.com>
2022-04-19 18:36:29 -0500

    added mailslurp-client to dependencies

--- backend/app.test.js
+++ backend/app.test.js
@@ -1,29 +1,25 @@
 const request = require("supertest");
 const app = require("../backend/app");
-const mongoose = require("mongoose")
-const { MailSlurp } = require("mailslurp-client")
-const MAILSLURP_KEY = process.env.MAILSLURP_KEY
+const mongoose = require("mongoose");
+const { MailSlurp } = require("mailslurp-client");
+const MAILSLURP_KEY = process.env.MAILSLURP_KEY;
 
-afterAll(done => {
-  mongoose.connection.close()
-  done()
-})
+afterAll((done) => {
+  mongoose.connection.close();
+  done();
+});
 
 test("It should response the GET method from '/'", async () => {
   const response = await request(app).get("/");
   expect(response.statusCode).toBe(200);
 });
-  
+
 test("It should response the GET method from '/products'", async () => {
-    return await request(app)
-      .get("/products")
-      .expect(200)
+  return await request(app).get("/products").expect(200);
 });
 
 describe("registration and login", () => {
-  it("can sign up as a new user", async () => {
-
-  })
-}) 
+  it("can sign up as a new user", async () => {});
+});
 
 // add test for mongoDB Connection
